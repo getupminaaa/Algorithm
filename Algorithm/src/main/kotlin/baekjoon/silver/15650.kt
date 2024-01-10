ffef5ad6f5d12b58aecadc5571af569b72ddb317
getupminaaa <getupmina0405@gmail.com>
2024-01-10 14:51:15 +0900

    refactor: 필요없는 변수 제거

--- Algorithm/src/main/kotlin/baekjoon/silver/15650.kt
+++ Algorithm/src/main/kotlin/baekjoon/silver/15650.kt
@@ -6,18 +6,17 @@ import java.io.InputStreamReader
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val (n, m) = readLine().split(" ").map { it.toInt() }
     val arr = mutableListOf<Int>()
-    val visited = Array(n + 1) { false }
-    back(arr, visited, n, m, 0)
+    back(arr, n, m, 0)
 }
 
-private fun back(arr: MutableList<Int>, visited: Array<Boolean>, n: Int, m: Int, s: Int) {
+private fun back(arr: MutableList<Int>, n: Int, m: Int, s: Int) {
     if (arr.size == m) {
         println(arr.joinToString(" "))
         return
     }
     for (i in s + 1..n) {
         arr.add(i)
-        back(arr, visited, n, m, i)
+        back(arr, n, m, i)
         arr.removeLast()
     }
 }
--- Algorithm/src/main/kotlin/baekjoon/silver/15652.kt
+++ Algorithm/src/main/kotlin/baekjoon/silver/15652.kt
@@ -8,19 +8,18 @@ private val sb = StringBuilder()
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val (n, m) = readLine().split(" ").map { it.toInt() }
     val arr = mutableListOf<Int>()
-    val visited = Array(n + 1) { false }
-    back(arr, visited, n, m, 1)
+    back(arr, n, m, 1)
     print(sb)
 }
 
-private fun back(arr: MutableList<Int>, visited: Array<Boolean>, n: Int, m: Int, s: Int) {
+private fun back(arr: MutableList<Int>, n: Int, m: Int, s: Int) {
     if (arr.size == m) {
         sb.append(arr.joinToString(" ")).append("\n")
         return
     }
     for (i in s ..n) {
         arr.add(i)
-        back(arr, visited, n, m, i)
+        back(arr, n, m, i)
         arr.removeLast()
     }
 }
