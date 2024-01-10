577f530af1eefad299390c005bee8de51be6d9ee
getupminaaa <getupmina0405@gmail.com>
2024-01-10 13:03:38 +0900

    baekjoon 15652번

--- Algorithm/src/main/kotlin/baekjoon/silver/15652.kt
+++ Algorithm/src/main/kotlin/baekjoon/silver/15652.kt
@@ -2,20 +2,23 @@ package baekjoon.silver
 
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.lang.StringBuilder
 
+private val sb = StringBuilder()
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
     val (n, m) = readLine().split(" ").map { it.toInt() }
     val arr = mutableListOf<Int>()
     val visited = Array(n + 1) { false }
-    back(arr, visited, n, m, 0)
+    back(arr, visited, n, m, 1)
+    print(sb)
 }
 
 private fun back(arr: MutableList<Int>, visited: Array<Boolean>, n: Int, m: Int, s: Int) {
     if (arr.size == m) {
-        println(arr.joinToString(" "))
+        sb.append(arr.joinToString(" ")).append("\n")
         return
     }
-    for (i in s + 1..n) {
+    for (i in s ..n) {
         arr.add(i)
         back(arr, visited, n, m, i)
         arr.removeLast()
