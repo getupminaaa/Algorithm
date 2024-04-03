5f1daecdbf1ff584f7991bf028ade5724b0ac6e6
getupminaaa <getupmina0405@gmail.com>
2024-04-03 14:57:36 +0900

    baekjoon 2660번

---
+++ Algorithm/src/main/kotlin/baekjoon/gold/2660.kt
@@ -0,0 +1,63 @@
+package baekjoon.gold
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.StringBuilder
+import java.util.*
+
+private var n = 0 //회원 수
+private lateinit var friendsInfo: MutableList<MutableList<Int>>
+private lateinit var scoreInfo: MutableList<Int>
+private val sb = StringBuilder()
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    n = readLine().toInt()
+
+    //친구 정보를 담는 이차원 배열
+    friendsInfo = MutableList(n + 1) { mutableListOf<Int>() }
+    scoreInfo = MutableList(n + 1) { Int.MAX_VALUE }
+
+    //입력을 계속 받다가 -1,-1이 들어오면 while문 밖으로 나온다.
+    while (true) {
+        val temp = readLine().split(" ").map { it.toInt() }
+        if (temp == mutableListOf(-1, -1)) break
+        friendsInfo[temp[0]].add(temp[1])
+        friendsInfo[temp[1]].add(temp[0])
+    }
+
+    repeat(n) {
+        bfs(it + 1)
+    }
+    val min = scoreInfo.min()
+    val num = scoreInfo.count { it == min }
+    val candidates = mutableListOf<Int>()
+    scoreInfo.forEachIndexed { index, i ->
+        if(i == min){
+            candidates.add(index)
+        }
+    }
+    sb.append("$min $num").append("\n")
+    candidates.sorted().forEach { sb.append(it).append(" ") }
+
+    println(sb)
+}
+
+private fun bfs(start: Int) {
+    val queue: Queue<Int> = LinkedList()
+    val visited = MutableList(n + 1) { -1 }
+    //queue에 처음 넣고
+    queue.add(start)
+    visited[start] = 0
+    //while문 안에서
+    //queue 에 있는거 빼서 갖고 있다가
+    //뺴서 갖고 있는 그거랑 연결되어있는것들 방문
+    while (queue.isNotEmpty()) {
+        val p = queue.poll()
+        friendsInfo[p].forEach {
+            if (visited[it] == -1) {
+                queue.add(it)
+                visited[it] = visited[p] + 1
+            }
+        }
+    }
+    scoreInfo[start] = visited.max()
+}
